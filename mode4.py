6f10dd9d84c01870b1809da100530a4cc3999a57
mc kim <dies53166@naver.com>
2022-06-01 17:50:51 +0900

    06/01 - 18:00

--- mode4.py
+++ mode4.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-def mat_inp(r, c):
+def mat_inp(r, c): #matrix 사이즈 입력받고 matrix의 원소들 입력받아 matrix return
     mat = np.zeros((r,c))
 
     s = "   "
@@ -19,17 +19,22 @@ def mat_inp(r, c):
 
     for row in range(len(mat)):
         for col in range(len(mat[row])):
-            mat[row, col] = eval(input(f"[{row}, {col}] = ")) #eval => mode1
+            mat[row, col] = eval(input(f"[{row}, {col}] = ")) #eval예외처리
             
     return mat
 
 
-def menu1(mat):
-    if mat[0,0] == 0 and mat[0,1] ==0 and mat[0,2] != 0:
+def menu1(mat): #2X3행렬 입력받아서 X,Y값을 "X = {X값}, Y = {Y값}"의 형태의  string으로 return
+    '''
+    2X3행렬 (Ax =b)
+    a1x + b1y = c1
+    a2x + b2y = c2
+    '''
+    if mat[0,0] == 0 and mat[0,1] ==0 and mat[0,2] != 0: # a1,b1 = 0이고 c1은 0이 아닌경우
         print("Math Error")
-    elif mat[1,0] == 0 and mat[1,1] ==0 and mat[1,2] != 0:
+    elif mat[1,0] == 0 and mat[1,1] ==0 and mat[1,2] != 0: # a2,b2 = 0이고 c2은 0이 아닌경우
         print("Math Error")
-    elif mat[0,0]*mat[1,1] == mat[1,0]*mat[0,1] and mat[0,2] != mat[1,2]:
+    elif mat[0,0]*mat[1,1] == mat[1,0]*mat[0,1] and mat[0,2] != mat[1,2]: #a1*b2 = a2*b1 이지만 c1 != c2 인경우
         print("Math Error")        
     else:
         x = round((mat[0,2]*mat[1,1] - mat[1,2]*mat[0,1]) / (mat[0,0]*mat[1,1] - mat[1,0]*mat[0,1]),9)
@@ -38,7 +43,13 @@ def menu1(mat):
         return result
 
 
-def menu2(mat):
+def menu2(mat): #3X4행렬 입력받아서 X,Y값을 "X = {X값}, Y = {Y값}, Z = {Z값}"의 string으로 return
+    '''
+    3X4행렬 (Ax = b)
+    a1x + b1y + c1z = d1
+    a2x + b2y + c2z = d2
+    a3x + b3y + c3z = d3
+    '''
     mat_eq = np.zeros((3,3))
     mat_d = np.zeros((3,1))
     for i in range(3):
@@ -47,16 +58,21 @@ def menu2(mat):
             mat_eq[i,j] = mat[i,j]
 
 
-    mat_eq_inv = np.linalg.inv(mat_eq)
+    mat_eq_inv = np.linalg.inv(mat_eq) 
 
-    ans = np.dot(mat_eq_inv,mat_d)
+    ans = np.dot(mat_eq_inv,mat_d) # x = (A 역행렬) * b
     
     print(ans)
     result = f"X = {round(ans[0][0],9)}, Y = {round(ans[1][0],9)}, Z = {round(ans[2][0],9)}"
     return result
 
 
-def menu3(mat):
+def menu3(mat): #3X1행렬 입력받아서 X,Y값을 "X1 = {X1값}, X2 = {X2값}"의 string으로 return
+    '''
+    3X1행렬
+    ax^2 + bx + c = 0
+
+    '''
     a = mat[0][0]
     b = mat[0][1]
     c = mat[0][2]
@@ -66,21 +82,21 @@ def menu3(mat):
         print("Math Error")
 
     else:
-        dis = b**2-4*a*c
+        dis = b**2-4*a*c #판별식
 
-        if dis == 0:
+        if dis == 0: # 중근
             x = -b/(2*a)
             result = f"X = {round(x,9)}"
 
-        elif dis>0:
+        elif dis>0: # 서로 다른 두 실근
             x1 = (-b + dis**0.5)/(2*a)
             x2 = (-b - dis**0.5)/(2*a)
             result = f"X1 = {round(x1,9)}, X2 = {round(x2,9)}"
 
-        else:
+        else:# 서로 다른 두 허근
             real = round(-b/(2*a),9)
             imag = ((-dis)**0.5)/(2*a)
-            imag = round(imag, 9)
+            imag = round(imag, 9)# 허수부 반올림
             x1 = real + imag*1j
             x2 = real - imag*1j
             result = f"X1 = {x1}, X2 = {x2}"
@@ -88,30 +104,35 @@ def menu3(mat):
         return result
     
 
-def func(a, b, c, d, x):
+def func(a, b, c, d, x):#3차식의 계수와 x값을 입력받아 결과 return
     return (a * (x**3)) + (b * (x**2)) + (c * x) + d
 
 
 def cubic(a,b,c,d):
-    n = -b**3/27/a**3 + b*c/6/a**2 - d/2/a
-    s = (n**2 + (c/3/a - b**2/9/a**2)**3)**0.5
-    r0 = (n-s)**(1/3)+(n+s)**(1/3) - b/3/a
-    r1 = (n+s)**(1/3)+(n+s)**(1/3) - b/3/a
-    r2 = (n-s)**(1/3)+(n-s)**(1/3) - b/3/a
-    return (r0,r1,r2)
+    n = 2*(b**3)-9*a*b*c+27*(a**2)*d
+    s = ((n**2 - (4*(b**2-3*a*c)**3)))**0.5
+    r0 =  -(b/3/a)-(1/3/a)*(((n+s)/2)**(1/3)+((n-s)/2)**(1/3))
+    r1 = -(b/3/a)+((1+(3**0.5)*1j)/6/a)*(((n+s)/2)**(1/3))+((1-(3**0.5)*1j)/6/a)*(((n-s)/2)**(1/3))
+    r2 = -(b/3/a)+((1-(3**0.5)*1j)/6/a)*(((n+s)/2)**(1/3))+((1+(3**0.5)*1j)/6/a)*(((n-s)/2)**(1/3))
+    return r0,r1,r2
 
 
-def menu4(mat):
-    a = mat[0][0]
-    b = mat[0][1]
-    c = mat[0][2]
-    d = mat[0][3]
+def menu4(mat):#4X1행렬 입력받아서 X,Y값을 "X1 = {X1값}, X2 = {X2값}, X3 = {X3값}"의 string으로 return
+    '''
+    4X1행렬
+    ax^3 + bx^2 + cx + d = 0
+
+    '''
+    a = float(mat[0][0])
+    b = float(mat[0][1])
+    c = float(mat[0][2])
+    d = float(mat[0][3])
 
     if a == 0:
         print("Math Error")
 
     else:
-        '''
+        
         x1, x2, x3 = cubic(a,b,c,d)
         root = {x1, x2, x3}
         root_temp = list(root)
@@ -119,96 +140,26 @@ def menu4(mat):
         result = ""
         for i in range(len(root)):
             x = "x" + f"{i+1}"
-            result += f"X{i+1} = " + str(round(root_temp[i],9))
-            if i != len(root)-1:
-                result += ", "
-        '''
-
-        p = 0
-        q = 0
-        while(True):
-            r1 = random.randint(-100,100)
-            r2 = random.randint(-100,100)
-
-            if func(a, b, c, d, r1)*func(a, b, c, d, r2) <0:
-                p, q = r1, r2
-                break
-
-        m = (p+q)/2
-        error = abs(func(a,b,c,d,m))
-
-        while error != 0:
-            if func(a,b,c,d,m)*func(a,b,c,d,p) < 0:
-                q = m
-            else:
-                p = m
-            m_temp = m
-            m = (p+q)/2
-            if m_temp == m:
-                break
-            error = abs(func(a,b,c,d,m))
-
-        x1 = 0
-
-        if (round(m) == round(m,2) and round(m) == round(m,3) and round(m) == round(m,4) and round(m) == round(m,5)):
-            x1 = round(m, 4)
-        else:
-            x1 = round(m, 9)
-
-        
-        b, c = b+a*x1, c+(b+a*x1)*x1
-        dis = (b**2)-4*a*c
-
-        if dis == 0:
-            x2 = -b/(2*a)
-            if (round(x2) == round(x2,2) and round(x2) == round(x2,3) and round(x2) == round(x2,4) and round(x2) == round(x2,5)):
-                x2 = round(x2, 4)
-                e
-            else:
-                x2 = round(x2, 9)
-            if x1 == x2:
-                result = f"X = {x1}"
-            else :
-                result = f"X1 = {x1}, X2 = {round(x2,9)}"
-                
-
-        elif dis>0:
-            x2 = (-b + dis**0.5)/(2*a)
-            x3 = (-b - dis**0.5)/(2*a)
-            if (round(x2) == round(x2,2) and round(x2) == round(x2,3) and round(x2) == round(x2,4) and round(x2) == round(x2,5)):
-                x2 = round(x2, 4)
-            else:
-                x2 = round(x2, 9)
-            if (round(x3) == round(x3,2) and round(x3) == round(x3,3) and round(x3) == round(x3,4) and round(x3) == round(x3,5)):
-                x3 = round(x3, 4)
-            else:
-                x3 = round(x3, 9)
-            if x1 == x2:
-                result = f"X1 = {x1}, X2 = {round(x3,9)}"
-            elif x1 == x3:
-                result = f"X1 = {x1}, X2 = {round(x2,9)}"
-            else:
-                result = f"X1 = {x1}, X2 = {round(x2,9)}, X3 = {round(x3,9)}"
-
-        else:
-            real = -b/(2*a)
-            if (round(real) == round(real,2) and round(real) == round(real,3) and round(real) == round(real,4) and round(real) == round(real,5)):
-                real = round(real, 4)
-            else:
-                real = round(real, 9)
-
-            imag = ((-dis)**0.5)/(2*a)
-            if (round(imag) == round(imag,2) and round(imag) == round(imag,3) and round(imag) == round(imag,4) and round(imag) == round(imag,5)):
-                imag = round(imag, 4)
-            else:
-                imag = round(imag, 9)
-            x2 = real + imag*1j
-            x3 = real - imag*1j
-            result = f"X1 = {x1}, X2 = {x2}, X3 = {x3}"
+            x = eval(x)
+            
+            if isinstance(x,int) or isinstance(x,float):
+                x = round(x,9)
+                result = f"X{i+1} = " + f"{x}"
     
-
+            elif isinstance(x, complex):
+                real = x.real
+                imag = x.imag
+                real = round(real, 9)
+                imag = round(imag, 9)
+            
+                result += f"X{i+1} = " + f"{real}"
+                if imag != 0:
+                    result += f"{imag}j"
+                if i != len(root)-1:
+                    result += ", "
 
         return result
+
 
 def mode4(oname):
     ans = None
